cf06f3177ee3aa0459ce7971d3fccfc58ea1c8d4
Arthur <arthurg.gmarques@gmail.com>
2025-04-15 19:56:48 -0300

    Importado as rotas para o Router.
    Testar as rotas e métodos no Postman.

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,14 @@
 import clientes from "./ClientesRouter.js"
+import Lanches from "./LanchesRouter.js"
+import Bebidas from "./BebidasRouter.js"
+import Combos from "./CombosRouter.js"
+import Enderecos from "./EnderecosRouter.js"
+import Pedidos from "./PedidosRouter.js"
 
 const Routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"));
   
-    app.use(clientes);
+    app.use(clientes, Lanches, Bebidas, Combos, Enderecos, Pedidos);
 };
 
 export default Routes
